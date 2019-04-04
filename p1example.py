2019c8931aeae4f9f8a43075e0e2c312abbaf4e6
aaronarcade <AaronBBrown@gatech.edu>
2019-04-04 19:22:30 -0400

    Added bolt constraints
    doesn't work - adding the bolts breaks the code, results in unbounded error

--- p1example.py
+++ p1example.py
@@ -11,10 +11,6 @@ nodes = demandData.index.tolist()
 demands = demandData.to_dict()['Demand']
 group = demandData.to_dict()['Resident Group Number']
 
-print(nodes)
-print(demands)
-print(group)
-
 N = len(nodes) + 1
 Ngroups = 0
 for i in nodes:
@@ -34,6 +30,7 @@ print("Total demand by group: ", groupdemand)
 
 arcData = pd.read_csv("DS9_Network_Arc_Data_B2.csv", header = 0)
 arcs, capacity = multidict({(row[0], row[1]) : row[2] for (index, row) in arcData.iterrows()})
+fixarcs, maximp = multidict({(row[0], row[1]) : row[3] for (index, row) in arcData.iterrows()})
 
 #Rudimentary data processing to transform into a max flow network
 if biderictional:
@@ -54,21 +51,28 @@ arcs, capacity = multidict(capacity)
 
 m = Model('netflow')
 
-# Create variables
+# Create variables (flow between nodes, number of bolts added to arcs, whether or not bolts are added)
 flow = m.addVars(arcs, name="flow")
-#binary set
-#integer set
+bolts = m.addVars(arcs, vtype=GRB.INTEGER, name="bolts")
+fixed = m.addVars(arcs, vtype=GRB.BINARY, name="fixed")
 
 #Set objective
 m.setObjective(flow.sum(source,'*'), GRB.MAXIMIZE)
 
 # Arc capacity constraints
 m.addConstrs(
-    (flow[i,j] <= capacity[i,j] for i,j in arcs), "cap")
+   (flow[i,j]  <= capacity[i,j] for i,j in arcs), "cap")
 
 # Flow conservation constraints
 m.addConstrs(
-    (flow.sum('*',j) == flow.sum(j,'*') for j in nodes), "node")
+    (flow.sum('*',j) == flow.sum(j,'*') for j in nodes), "node")  
+
+# va >= 0
+m.addConstrs(
+    (bolts[i,j] >= 0 for i,j in fixarcs), "pos bolt")
+
+m.addConstrs(
+	(bolts[i,j] <= maximp[i,j]*fixed[i,j] for i,j in fixarcs), "max")
 
 # Compute optimal solution
 m.write("project.lp")
@@ -93,3 +97,5 @@ if m.status == GRB.Status.OPTIMAL:
     for g in range(Ngroups):
         if groupdemand[g] > 0:
             print("Group %s had proportion %g of its demand satisfied" % (g+1, groupsat[g]/groupdemand[g]))
+            
+
